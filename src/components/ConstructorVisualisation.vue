a09d17278468e13456432641f8883e89344b5021
rakushka-san <vladislavmohov@mail.ru>
2023-09-09 17:05:37 +0300

    Блок с изображением конструктора

---
+++ src/components/ConstructorVisualisation.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="visualisation">
+		<img
+			src="./../assets/img/34t.png"
+			alt="AK Image"
+			class="visualisation__image"
+		/>
+		<img
+			src="./../assets/img/0.png"
+			alt="Extra Image"
+			class="visualisation__image visualisation__image_transparent"
+		/>
+		<img
+			src="./../assets/img/1.png"
+			alt="Extra Image"
+			class="visualisation__image"
+		/>
+		<img
+			src="./../assets/img/2.png"
+			alt="Extra Image"
+			class="visualisation__image"
+		/>
+		<img
+			src="./../assets/img/3.png"
+			alt="Extra Image"
+			class="visualisation__image"
+		/>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'ConstructorVisualisation',
+})
+</script>
+
+<style lang="scss">
+.visualisation {
+	position: relative;
+
+	&__image {
+		width: 100%;
+
+		position: absolute;
+
+		&_transparent {
+			opacity: 0.3;
+		}
+	}
+}
+</style>
